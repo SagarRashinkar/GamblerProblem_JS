7d8191988e79aa96b43ec9b4af8f1d3ec69870db
Sagar <sagarrashinkar2@gmail.com>
2021-02-10 17:14:33 +0530

    Added: Resign stake win or loose

--- Utility/Utility.js
+++ Utility/Utility.js
@@ -6,14 +6,24 @@ class Utility{
         return Math.floor(Math.random()*2);
     }
     gameGambler(){
-        let result = this.checkWinOrLoose();
-        if(result === 1){
-            console.log("Gambler Wins the game");
-            this.dayStake += 1;
+        while(this.dayStake !== 150 && this.dayStake !== 50){
+            let result = this.checkWinOrLoose();
+            if(result === 1){
+                console.log("Gambler Wins the game");
+                this.dayStake += 1;
+            }
+            if(result === 0){
+                console.log("Gambler Looses the game");
+                this.dayStake -= 1;
+            }
         }
-        if(result === 0){
-            console.log("Gambler Looses the game");
-            this.dayStake -= 1;
+        if(this.dayStake == 150){
+            console.log("Gambler wins 50% of the stake");
+            console.log("After winning total amount is: ");
+        }
+        else{
+            console.log("Gambler loose 50% of the stake");
+            console.log("After loosing total amount is: ");
         }
         console.log(this.dayStake);
     }
