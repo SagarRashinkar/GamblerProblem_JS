ef3fda1cebc22af2216a231a8c6a8cd7517e7b29
Sagar <sagarrashinkar2@gmail.com>
2021-02-10 18:33:21 +0530

    Added: win days and lost days in month

--- Utility/Utility.js
+++ Utility/Utility.js
@@ -4,6 +4,8 @@ class Utility{
     day =1;
     winAmt = 0;
     looseAmt = 0;
+    winDay = 0;
+    lostDay = 0;
 
     checkWinOrLoose(){
         return Math.floor(Math.random()*2);
@@ -12,14 +14,17 @@ class Utility{
         while(this.dayStake !== 150 && this.dayStake !== 50){
 
             let result = this.checkWinOrLoose();
+
             if(result === 1){
                 console.log("Gambler Wins the game");
                 this.dayStake += 1;
 
-                if(this.day<=20){
+                if(this.day<=30){
                     this.winAmt += 1;
+                    this.winDay +=1;
                 }
                 this.day += 1;
+
             }
             if(result === 0){
                 console.log("Gambler Looses the game");
@@ -27,6 +32,7 @@ class Utility{
 
                 if(this.day<=20){
                     this.looseAmt -= 1;
+                    this.lostDay += 1;
                 }
                 this.day += 1;
             }
@@ -41,6 +47,9 @@ class Utility{
         }
         console.log("After 20 days Total Amount Won is: "+this.winAmt);
         console.log("After 20 days Total Amount Lost is: "+this.looseAmt);
+
+        console.log("Total Days Won are: "+this.winDay);
+        console.log("Total Days Lost are: "+this.lostDay);
     }
 }
 
