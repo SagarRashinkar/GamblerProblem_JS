2f481dc92723456a46de498a34f54fe398202dd6
Sagar <sagarrashinkar2@gmail.com>
2021-02-12 01:27:02 +0530

    Added: LuckyUnLucky Days

--- Gambler.js
+++ Gambler.js
@@ -1,3 +1,4 @@
 const util = require('./Utility/Utility');
 
 util.gameGambler();
+util.checkLuckyUnlucky();
--- Utility/Utility.js
+++ Utility/Utility.js
@@ -6,7 +6,7 @@ class Utility{
     looseAmt = 0;
     winDay = 0;
     lostDay = 0;
-
+    
     checkWinOrLoose(){
         return Math.floor(Math.random()*2);
     }
@@ -14,29 +14,37 @@ class Utility{
         while(this.dayStake !== 150 && this.dayStake !== 50){
 
             let result = this.checkWinOrLoose();
+            this.winOrLoose(result);
 
-            if(result === 1){
-                console.log("Gambler Wins the game");
-                this.dayStake += 1;
-
-                if(this.day<=30){
-                    this.winAmt += 1;
-                    this.winDay +=1;
-                }
-                this.day += 1;
-
-            }
-            if(result === 0){
-                console.log("Gambler Looses the game");
-                this.dayStake -= 1;
-
-                if(this.day<=20){
-                    this.looseAmt -= 1;
-                    this.lostDay += 1;
-                }
-                this.day += 1;
-            }
         }
+        this.checkDayStake();
+    }
+
+    winOrLoose(result){
+        
+        if(result === 1){
+            console.log("Gambler Wins the game");
+            this.dayStake += 1;
+
+            if(this.day<=30){
+                this.winAmt += 1;
+                this.winDay +=1;
+            }
+            this.day += 1;
+
+        }
+        if(result === 0){
+            console.log("Gambler Looses the game");
+            this.dayStake -= 1;
+
+            if(this.day<=30){
+                this.looseAmt -= 1;
+                this.lostDay += 1;
+            }
+            this.day += 1;
+        }
+    }
+    checkDayStake(){
         if(this.dayStake == 150){
             console.log("Gambler wins 50% of the stake");
             console.log("After winning total amount is: "+this.dayStake);
@@ -50,6 +58,25 @@ class Utility{
 
         console.log("Total Days Won are: "+this.winDay);
         console.log("Total Days Lost are: "+this.lostDay);
+    }
+    checkLuckyUnlucky(){
+        let luckyDay = [];
+        let unluckyDay = [];
+
+        for(var i=1; i<=30; i++){
+            let res = this.checkWinOrLoose();
+            if(res === 0){
+                unluckyDay.push(i);
+            }
+            if(res === 1){
+                luckyDay.push(i);
+            }
+        }
+
+        console.log("Max Days Win");
+        console.log(luckyDay.forEach(wday => {console.log(wday)}));
+        console.log("Max Days Loose: ");
+        console.log(unluckyDay.forEach(lday=> {console.log(lday)}));
     }
 }
 
